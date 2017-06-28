8b38dcebb7828bf9d7262732e35ecf50c0ee5e81
rex64 <rex64@users.noreply.github.com>
2017-06-28 22:00:34 +0200

    some refactoring

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 import tdl
 
 
+class Object:
+
+    def __init__(self, x, y, char='?', color=(255, 255, 255)):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+
+    def render(self, console):
+        console.draw_str(self.x, self.y, self.char, fg=self.color)
+
+
 class Game:
     
     def __init__(self, w, h):
         self.width = w
         self.height = h
-        
-        self.player_x = 0 
-        self.player_y = 0
-        
         self.console = console = tdl.init(w, h)
 
+        self.objects = []
+        self.player = Object(0, 0, '@')
+        self.objects.append(self.player)
+        self.objects.append(Object(5, 2, 'D'))
+
     def render(self):
-        self.console.clear()
+        self.console.clear(bg=(25, 25, 25))
         self.console.draw_str(1, self.height//2, "Hello r/roguelikedev !")
-        self.console.draw_str(self.player_x, self.player_y, "@", fg=(255, 0, 0))
-        
+        self.console.draw_frame(1,1,10,10,'x',(255,0,0))
+        for obj in self.objects:
+            obj.render(self.console)
+
         tdl.flush()
 
     def run(self):
-        
         while not tdl.event.is_window_closed():          
             for e in tdl.event.get():
                 if e.type == "KEYDOWN":
                     key = e.text.lower()
+                    x, y = 0, 0
                     if key == "w":
-                        self.player_y -= 1
+                        y = -1
                     if key == "a":
-                        self.player_x -= 1
+                        x = -1
                     if key == "s":
-                        self.player_y += 1
+                        y = 1
                     if key == "d":
-                        self.player_x += 1
+                        x = 1
+
+                    self.player.move(x, y)
 
             self.render()
             tdl.event.keyWait()
