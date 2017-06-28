a769f984a80c1ad1162a36b938ff3532919125bf
rex64 <rex64@users.noreply.github.com>
2017-06-28 20:07:18 +0200

    Player movements

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import tdl
 
 
+class Game:
+    
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+        
+        self.player_x = 0 
+        self.player_y = 0
+        
+        self.console = console = tdl.init(w, h)
+
+    def render(self):
+        self.console.clear()
+        self.console.draw_str(1, self.height//2, "Hello r/roguelikedev !")
+        self.console.draw_str(self.player_x, self.player_y, "@", fg=(255, 0, 0))
+        
+        tdl.flush()
+
+    def run(self):
+        
+        while not tdl.event.is_window_closed():          
+            for e in tdl.event.get():
+                if e.type == "KEYDOWN":
+                    key = e.text.lower()
+                    if key == "w":
+                        self.player_y -= 1
+                    if key == "a":
+                        self.player_x -= 1
+                    if key == "s":
+                        self.player_y += 1
+                    if key == "d":
+                        self.player_x += 1
+
+            self.render()
+            tdl.event.keyWait()
+            
+
 def main():
-    console = tdl.init(24, 16)
-    console.draw_str(1, 16//2, 'Hello r/roguelikedev !')
-    tdl.flush()
-    tdl.event.keyWait()
+    game = Game(24, 16)
+    game.run()
 
 
 if __name__ == "__main__":
